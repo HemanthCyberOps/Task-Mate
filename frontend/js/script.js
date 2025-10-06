713f5081d17b48b5e9bcd3a84e1c4ac0327f9e48
HemanthCyberOps <hemanthd1007@gmail.com>
2025-10-06 11:56:39 +0530

    Logic modification in js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -25,3 +25,38 @@ document.addEventListener("DOMContentLoaded", () =>{
     loginForm.style.display = "block";
     signupForm.style.display = "none";
 });
+
+function validateSignUpForm(){
+    const password = document.getElementById("signup-password").value;
+    const confirm_password = document.getElementById("signup-confirm-password").value;
+
+    if(password !== confirm_password){
+        alert("Password does not match!")
+        return false;
+    }
+
+    if(password.length < 6){
+        alert("Password must be at least 6 characters!")
+        return false;
+    }
+
+    return true;
+}
+
+function validateLoginForm(){
+    const email = document.getElementById("login-email").value.trim();
+    const password = document.getElementById("login-password").value.trim();
+
+    if(!email){
+        alert("Please enter your email!");
+        return false;
+    }
+
+    if(!password){
+        alert("Please Enter your password!");
+        return false;
+    }
+
+    return true;
+
+}
